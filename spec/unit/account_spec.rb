dfc35752e08de9114bcbd356d69799bbe5e06a3b
anaalta <suciuanamaria@yahoo.com>
2017-06-12 15:17:14 +0200

    saves withdrawls in array

--- spec/unit/account_spec.rb
+++ spec/unit/account_spec.rb
@@ -14,10 +14,23 @@ describe Account do
     expect(account.balance).to eq (400)
   end
 
+  it 'adds up deposits to balance' do
+    account.deposit(1000);
+    account.deposit(500);
+    expect(account.balance).to eq(1500)
+  end
+
   it 'saves the deposits and the dates in which they were made' do
     account.deposit(1000);
     account.deposit(200);
     expect(account.deposits).to eq ([{Date.today => 1000}, {Date.today => 200}])
+  end
+
+  it 'saves the withdrawals and the dates in which they were made' do
+    account.deposit(2000);
+    account.withdraw(200);
+    account.withdraw(300);
+    expect(account.withdrawals).to eq ([{Date.today => 200}, {Date.today => 300}])
   end
 
 end
