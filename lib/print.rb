282afdb69b82f22b12995b322e16e16dd699f61e
anaalta <suciuanamaria@yahoo.com>
2017-06-12 16:12:37 +0200

    no print module needed

--- lib/print.rb
+++
@@ -1,9 +0,0 @@
-require_relative 'account'
-module Print
-  def print_statement
-    sorted = account.withdrawals+account.deposits
-    sorted = sort_me.sort_by { |key, value| key }
-    sorted.each {|element| element}
-
-  end
-end
